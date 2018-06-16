ccdc67696d127043fc56ebb5de3a09efabe05410
Han Tran <htrstrive@gmail.com>
2018-06-16 15:49:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
+<<<<<<< HEAD
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
+=======
+var greetSpecialGuest = "Hello" + specialGuest + "!";
+>>>>>>> 6ee588aa40e1278a831f8985ba799f2d22450a56
 
-var topic = "space";
+var topic = 'Let\'s talk about space.';
 
 var conversation = `Let\'s talk about ${topic}.`;
